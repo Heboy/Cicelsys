eba5ad6ff952a0d00e60a51e4ab742cd29e297f0
Heboy <421814961@qq.com>
2013-11-13 18:58:18 +0800

    修复侧边导航栏定位bug

--- public/script/index.js
+++ public/script/index.js
@@ -4,37 +4,48 @@
 
 var lgnForm = $('#lgn');
 var regForm = $('#reg');
+var regBtn =  $('#regBtn');
+var lgnBtn =  $('#lgnBtn');
 var obj = {
 	timer: null,
 	alpha: 1
 };
 
-$('#regBtn').on('click',{from:'reg'}, exchangeForm);
-$('#lgnBtn').on('click',{from:'lgn'}, exchangeForm);
+regBtn.on('click',{from:'reg'}, exchangeForm);
 
 function exchangeForm(e) {
-	var hideObj = null;
-	var showObj = null;
+
 	if(e.data.from=='reg')
 	{
-		hideObj =lgnForm;
-		showObj =regForm;
+		regBtn.off('click',exchangeForm);
+		obj.alpha = 1;
+		obj.timer = setInterval(function () {
+			obj.alpha -= 0.3;
+			lgnForm.css({opacity:obj.alpha});
+			if (obj.alpha < 0) {
+				clearInterval(obj.timer);
+				lgnForm.css('display', 'none');
+				regForm.css('display', 'block');
+				lgnForm.css({opacity:1});
+				lgnBtn.on('click',{from:'lgn'}, exchangeForm);
+			}
+		}, 100)
 	}
 	else if(e.data.from=='lgn')
 	{
-		hideObj =regForm;
-		showObj =lgnForm;
+		lgnBtn.off('click',exchangeForm);
+		obj.alpha = 1;
+		obj.timer = setInterval(function () {
+			obj.alpha -= 0.3;
+			regForm.css({opacity:obj.alpha});
+			if (obj.alpha < 0) {
+				clearInterval(obj.timer);
+				regForm.css('display', 'none');
+				lgnForm.css('display', 'block');
+				regForm.css({opacity:1});
+				regBtn.on('click',{from:'reg'}, exchangeForm);
+			}
+		}, 100)
 	}
-	obj.alpha = 1;
-	obj.timer = setInterval(function () {
-		obj.alpha -= 0.3;
-		hideObj.css({opacity:obj.alpha});
-		if (obj.alpha < 0) {
-			clearInterval(obj.timer);
-			hideObj.css('display', 'none');
-			showObj.css('display', 'block');
-			hideObj.css({opacity:1});
-		}
-	}, 100)
 }
 
--- public/script/overview.js
+++ public/script/overview.js
@@ -27,8 +27,10 @@ Ext.onReady(function () {
 
 //		生成概览图表
 	store.on('load', function (store, records, successful) {
+		var coursesObjArr = [];
 		var table = $('<table class="table table-striped table-bordered"></table>');
 		for (var j in records) {
+			coursesObjArr.push(records[j].data);
 			if(!title){
 				var title = $('<tr></tr>');
 				for(var i in records[j].data){
@@ -46,8 +48,11 @@ Ext.onReady(function () {
 			}
 			table.append(tr);
 		}
-		$('#overview').append(table);
+		$('#t0').append(table);
+		pag2(coursesObjArr);
 	})
+
+	pag3();
 
 	Ext.create('Ext.chart.Chart', {
 		renderTo: 't1',
@@ -129,4 +134,144 @@ Ext.onReady(function () {
 		],
 		renderTo: 't2'
 	});
+
+
+
+	function pag2(coursesObjArr) {
+		var series = [];
+		var fields = [
+			{name: "日期", type: "string"}
+		];
+		var chartFields = [];
+		for (var i = 0; i < coursesObjArr.length; i++) {
+			fields.push({name: coursesObjArr[i]['课程名称'], type: 'int'});
+			chartFields.push(coursesObjArr[i]['课程名称']);
+			series.push({
+				type: 'line',
+				axis: 'left',
+				highlight: true,
+				tips: {
+					trackMouse: true,
+					width: 25,
+					height: 15,
+					renderer: function (storeItem, item) {
+						this.setTitle(String(item.value[1]) + '人');
+					}
+				},
+				smooth: true,
+				xField: '日期',
+				yField: coursesObjArr[i]['课程名称'],
+				markerConfig: {
+					type: 'cross',
+					size: 4,
+					radius: 4,
+					'stroke-width': 0
+				}
+			})
+		}
+		var model = Ext.define('Course', {
+			extend: 'Ext.data.Model',
+			fields: fields
+		});
+
+		var store = Ext.create('Ext.data.JsonStore', {
+			model: model,
+			proxy: {
+				type: 'ajax',
+				url: '/Cicelsys/RemoteData/courses_data_trend.js',
+				reader: {
+					type: 'json'
+				}
+			},
+			autoLoad: true
+		});
+
+		Ext.create('Ext.chart.Chart', {
+			width: '100%',
+			height: 400,
+			store: store,
+			legend: {
+				position: 'right'
+			},
+			axes: [
+				{
+					type: 'Category',
+					position: 'bottom',
+					fields: ['日期']
+				},
+				{
+					type: 'Numeric',
+					position: 'left',
+					fields: chartFields
+				}
+			],
+			series: series,
+			renderTo: 'trend'
+		})
+	}
+
+	function pag3() {
+		var model = Ext.define('Course', {
+			extend: 'Ext.data.Model',
+			fields: [
+				{name: '课程名称', type: 'string'},
+				{name: '0点-4点', type: 'int', defaultValue: 0},
+				{name: '4点-8点', type: 'int', defaultValue: 0},
+				{name: '8点-12点', type: 'int', defaultValue: 0},
+				{name: '12点-16点', type: 'int', defaultValue: 0},
+				{name: '16点-20点', type: 'int', defaultValue: 0},
+				{name: '20点-24点', type: 'int', defaultValue: 0}
+			]
+		})
+		var store = Ext.create('Ext.data.JsonStore', {
+			model: model,
+			proxy: {
+				type: 'ajax',
+				url: '/Cicelsys/RemoteData/courses_visittime.js',
+				reader: {
+					type: 'json'
+				}
+			},
+			autoLoad: true
+		});
+
+		Ext.create('Ext.chart.Chart', {
+			width: '100%',
+			height: 400,
+			store: store,
+			legend: {
+				position: 'right'
+			},
+			axes: [
+				{
+					type: 'Numeric',
+					position: 'bottom',
+					fields: ['0点-4点', '4点-8点', '8点-12点', '12点-16点', '16点-20点', '20点-24点']
+				},
+				{
+					type: 'Category',
+					position: 'left',
+					fields: ['课程名称']
+				}
+			],
+			series: [
+				{
+					type: 'bar',
+					axis: 'bottom',
+					highlight: true,
+					tips: {
+						trackMouse: true,
+						width: 65,
+						height: 28,
+						renderer: function (storeItem, item) {
+							this.setTitle(String(item.value[1]) + '人');
+						}
+					},
+					xField: '课程名称',
+					yField: ['0点-4点', '4点-8点', '8点-12点', '12点-16点', '16点-20点', '20点-24点']
+				}
+			],
+			renderTo: 'time'
+		});
+	}
 })
