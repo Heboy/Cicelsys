0ab7a6416f197d56a0c2e3796c52420b05773ea6
Heboy <421814961@qq.com>
2013-11-19 17:01:49 +0800

    下雨啦

--- app.js
+++ app.js
@@ -12,20 +12,16 @@ var path = require('path');
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 3000);
+app.set('port', 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
-app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser('thsnxkd'));
+app.use(express.cookieSession());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
 
 app.get('/', routes.index);
 app.get('/course',course.courseData);
@@ -33,7 +29,7 @@ app.get('/chapter',course.courseData);
 app.post('/data/course',course.getCourseData);
 app.post('/data/chapter',course.getChapterData);
 
-http.createServer(app).listen(app.get('port'), function(){
+var server = http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
-
 });
+
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,20 @@
-
 /*
  * GET home page.
  */
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.index = function (req, res, next) {
+//	判断COOKIE是否存在
+//	存在的话直接跳转到登陆后首页
+//	不存在的话显示未登录首页
+	if(req.cookies.user){
+		exports.index2(req, res, next);
+	}
+	else{
+		res.render('index', { title: '没有COOKIE' });
+	}
 };
+
+exports.index2 = function(req,res,next){
+	console.log(req.cookies.user);
+	res.render('index',{title:'COOKIE!'});
+}
