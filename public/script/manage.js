657ec3b3cb017c45b70401bfa04ba03fb6fdfdaf
Heboy <421814961@qq.com>
2013-12-04 15:14:20 +0800

    新增

--- public/script/manage.js
+++ public/script/manage.js
@@ -4,15 +4,17 @@
 /**
  * 实现修改 删除的逻辑
  */
-var uplete = (function () {
+var manageCourse = (function () {
+
 	var currentTarget = null;
 	var lastTarget = null;
 	$('#t0').on('click', function (e) {
 		currentTarget = $(e.target);
-		///////////////////////start删除逻辑////////////////////////////////
 		if (currentTarget.hasClass('modify')) {
 			$('#modify').modal();
+			modify('update', currentTarget);
 		}
+		///////////////////////start删除逻辑////////////////////////////////
 		else if (currentTarget.hasClass('delete')) {
 			if (currentTarget.hasClass('isAlert')) {
 				removeAlert(lastTarget);
@@ -21,7 +23,9 @@ var uplete = (function () {
 				if (lastTarget) {
 					removeAlert(lastTarget);
 				}
-				currentTarget.popover({html: true, title: '确认删除', content: '<button type="button" class="btn btn-danger btn-sm confirm">确认</button><button type="button" class="btn btn-default btn-sm cancel">取消</button>'});
+				currentTarget.popover({html: true, title: '确认删除', content: '<a>该课程所有记录将会消失，继续？</a>' +
+					'<button type="button" class="btn btn-danger btn-sm confirm">确认</button>' +
+					'<button type="button" class="btn btn-default btn-sm cancel">取消</button>'});
 				currentTarget.popover('show');
 				currentTarget.addClass('isAlert');
 				lastTarget = currentTarget;
@@ -31,7 +35,6 @@ var uplete = (function () {
 			removeAlert(lastTarget);
 			var tr = $(lastTarget.parents('tr')[0]);
 			var id = tr.find($('.id')).text();
-			console.log(id);
 			if (id) {
 				$.post('http://localhost:63342/Cicelsys/public/manage.html', {op: 'delete', id: id}, function (data) {
 					//删除操作
@@ -53,44 +56,73 @@ var uplete = (function () {
 			removeAlert(lastTarget);
 		}
 		///////////////////////end删除逻辑////////////////////////////////
+	});
+
+	/////////////////////////添加逻辑////////////////////////////////
+	$('#add').on('click', function (e) {
+		$('#modify').modal();
+		console.log('tang');
+		modify('add');
 	})
+
 	function removeAlert(target) {
 		target.popover('destroy');
 		target.removeClass('isAlert');
 	}
 
-	$('#modify').on('shown.bs.modal', function (e) {
-		var tr = $(currentTarget.parents('tr')[0]);
-
+	function modify(op, currentTarget) {
 		$('.confirm').on('click', function (e) {
 			$('#modify').modal('hide');
-			var id = tr.find($('.id')).text();
 			var courseName = $('#courseName').val();
 			var url = $('#url').val();
 			var department = $('#department').val();
-
 			if (courseName.split(' ').length > 0 && url.split(' ').length && department.split(' ').length) {
 				//修改操作
-				$.post('http://localhost:63342/Cicelsys/public/manage.html',
-					{op: 'update', id: id, coursename: courseName, url: url, department: department},
-					function (data) {
-						if (!data.result) {
-							//返回'true'作为成功标志
-							tr.empty();
-							//构造新的tr
-							tr.append("<td>" + id + "</td>" +
-								"<td>" + courseName + "</td>" +
-								"<td>" + url + "</td>" +
-								"<td>" + department + "</td>" +
-								"<td><button type='button' class='modify btn btn-primary btn-sm'>修改</button>" +
-								"<button type='button' class='delete btn btn-danger btn-sm'>删除</button></td>");
-						}
-					});//这里的url替换成处理删除操作的url
+				if (op === 'update') {
+					var tr = $(currentTarget.parents('tr')[0]);
+					var id = tr.find($('.id')).text();
+					$.post('http://localhost:63342/Cicelsys/public/manage.html',
+						{op: 'update', id: id, coursename: courseName, url: url, department: department},
+						function (data) {
+							if (data.result === 'true') {
+								//返回'true'作为成功标志
+								tr.empty();
+								//构造新的tr
+								tr.append("<td>" + id + "</td>" +
+									"<td>" + courseName + "</td>" +
+									"<td>" + url + "</td>" +
+									"<td>" + department + "</td>" +
+									"<td><button type='button' class='modify btn btn-primary btn-sm'>修改</button>" +
+									"<button type='button' class='delete btn btn-danger btn-sm'>删除</button></td>");
+							}
+							else{
+								alert('修改失败');
+							}
+						});
+				}
+				else if (op === 'add') {
+					var tbody = $('#t0').find('tbody');
+					$.post('http://localhost:63342/Cicelsys/public/manage.html',
+						{op: 'add', coursename: courseName, url: url, department: department},
+						function (data) {
+							if (data.id&&data.result==='true') {
+								//返回'true'作为成功标志
+								var id = data.id;
+								tbody.append("<tr class='main-data'><td>" + id + "</td>" +
+									"<td>" + courseName + "</td>" +
+									"<td>" + url + "</td>" +
+									"<td>" + department + "</td>" +
+									"<td><button type='button' class='modify btn btn-primary btn-sm'>修改</button>" +
+									"<button type='button' class='delete btn btn-danger btn-sm'>删除</button></td></tr>");
+							}
+						});
+				}
 			}
 			else {
 				//存在未填写的字段
 			}
+			$('.confirm').off();
 		})
-	})
+	}
 })();
 
