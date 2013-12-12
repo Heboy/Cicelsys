8389349fdfc0ba897988381280c0a2e6dadaff47
Heboy <421814961@qq.com>
2013-12-12 15:40:35 +0800

    新增前端模块

---
+++ public/script/Cicel/Cicel_1.2.js
@@ -0,0 +1,109 @@
+/**
+ * Created by Heboy on 13-12-12.
+ */
+function Cicel(courseID, chapterID) {
+	var url = 'http://localhost:63342';//服务器地址
+	var min = 1000;//时间间隔，毫秒
+	var timestamp = {
+		//时间节点，单位为min
+		data: [0, 1, 2, 3, 4, 5],
+		//弹出被使用过的时间节点
+		next: function () {
+			this.data.shift();
+			return this;
+		},
+		//把分钟转换为毫秒
+		interval: function () {
+			return (this.data[1] - this.data[0]) * min;
+		},
+		staytime: function () {
+			return this.data[0];
+		}
+	}
+	var courseID = courseID;
+	var chapterID = chapterID;
+	var xhr = createXHR();
+
+	function createXHR() {
+		if (typeof XMLHttpRequest != "undefined") {
+			return new XMLHttpRequest();
+		}
+		else if (typeof ActiveXObject != "undefined") {
+			if (typeof  arguments.callee.activeXString != "string") {
+				var versions = ["MSXML2.XMLHttp.6.0", "MXXML2.XMLHttp.3.0", "MXXML2.XMLHttp"],
+					i,
+					len;
+				for (i = 0, len = versions.length; i < len; i++) {
+					try {
+						new ActiveXObject((versions[i]));
+						arguments.callee.activeXString = versions[i];
+					}
+					catch (ex) {
+						console.log('createXHR has error');
+					}
+				}
+			}
+			else {
+				throw new Error("No xhr object available");
+			}
+		}
+	}
+
+	function createGUID() {
+		function S4() {
+			return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+		}
+
+		function NewGuid() {
+			return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
+		}
+
+		return NewGuid();
+	}
+
+	function stringify(obj){
+		console.log(JSON.stringify(obj));
+		var str = "";
+		for(var i in obj){
+			str+='"'+i+'":"'+obj[i]+'",';
+		}
+		str = '{'+str.slice(0,-1)+'}';
+		return str;
+	}
+
+	var getBasicInfo = (function(){
+		if (!courseID) {
+			throw new Error('课程ID未定义');
+		}
+		var guid = createGUID();
+		var starttime = new Date().getTime();
+		return function(){
+			return {
+				courseID: courseID,
+				chapterID: chapterID,
+				guid: guid,
+				starttime: starttime
+			}
+		}
+	})();
+
+	(function send(){
+		timestamp.next();
+		var basic = getBasicInfo();
+		basic.staytime = timestamp.staytime();
+		basic = stringify(basic);
+		if (!timestamp.data.length) {
+			return false;
+		};
+		console.log(basic);
+		xhr.open('post', url, true);
+		xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+		xhr.send(basic);
+		setTimeout(function () {
+			send();
+		}, timestamp.interval());
+	})();
+}
+
+Cicel(12,0);//courseID chapterID
+
